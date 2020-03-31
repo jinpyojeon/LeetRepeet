dc4c16f4f05c63cc912b7b024d56ac3e8974cb43
Mridul Ghanshala <mghansha@uwaterloo.ca>
2020-03-31 11:21:47 +0530

    Create 2 different flows if user is on leetcode or user is not on leetcode

--- background.js
+++ background.js
@@ -3,15 +3,3 @@
 // found in the LICENSE file.
 
 'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'leetcode.com'},
-      })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
--- popup.js
+++ popup.js
@@ -3,19 +3,30 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     message.innerText = request.source;
     let re = /^(.+?)\//;
     const username = re.exec(message.innerText.split('https://assets.leetcode.com/users/')[1])[1];
+    chrome.storage.sync.set({ "username": username }, function(){
+    });
   }
 });
 
 function onWindowLoad() {
-
-  var message = document.querySelector('#message');
-
-  chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+  chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }, function(tabs) {
+    var currentUrl= tabs[0].url;
+    if(currentUrl.startsWith("https://leetcode.com/")) {
+      document.getElementsByClassName("other")[0].style.display = "none";
+      var message = document.querySelector('#message');
+      chrome.tabs.executeScript(null, {
+        file: "getPagesSource.js"
+      }, function() {
+        if (chrome.runtime.lastError) {
+          message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+        }
+      });
+    }
+    else {
+      document.getElementsByClassName("leetcode")[0].style.display = "none";
     }
   });
 }

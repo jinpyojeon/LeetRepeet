3cc248c8b32bec05bb45ecd170b003a3d714443e
Mridul Ghanshala <mghansha@uwaterloo.ca>
2020-03-31 19:54:29 +0530

    Extract problem details

--- popup.js
+++ popup.js
@@ -1,55 +1,74 @@
 chrome.runtime.onMessage.addListener(function (request, sender) {
   // eslint-disable-next-line eqeqeq
-  if (request.action === 'getSource') {
+  if (request.action === "getSource") {
     message.innerText = request.source;
     const re = /^(.+?)\//;
-    const username = re.exec(message.innerText.split('https://assets.leetcode.com/users/')[1])[1];
+    const username = re
+      .exec(message.innerText.split("https://assets.leetcode.com/users/")[1])[1]
+      .trim();
+    console.log(username);
   }
 });
 
-function onWindowLoad () {
+function onWindowLoad() {
   chrome.tabs.query(
     {
       active: true,
-      lastFocusedWindow: true
+      lastFocusedWindow: true,
     },
     function (tabs) {
       var currentUrl = tabs[0].url;
-      if (currentUrl.startsWith('https://leetcode.com/')) {
-        document.getElementsByClassName('other')[0].style.display = 'none';
-        if (currentUrl.startsWith('https://leetcode.com/problems/')) {
-          document.getElementsByClassName('home')[0].style.display = 'none';
-            function modifyDOM() {
-              console.log('Tab script:');
-              console.log(document.body);
-              return document.body.innerHTML;
-            }
-            chrome.tabs.executeScript({
-              code: '(' + modifyDOM + ')();'
-            }, (results) => {
-              console.log(results[0]);
-            });
-        }
-        else {
-          document.getElementsByClassName('problems')[0].style.display = 'none';
-          var message = document.querySelector('#message');
-          chrome.tabs.executeScript(null,
+      if (currentUrl.startsWith("https://leetcode.com/")) {
+        document.getElementsByClassName("other")[0].style.display = "none";
+        if (currentUrl.startsWith("https://leetcode.com/problems/")) {
+          document.getElementsByClassName("home")[0].style.display = "none";
+          function modifyDOM() {
+            return document.body.innerHTML;
+          }
+          chrome.tabs.executeScript(
             {
-              file: 'getPagesSource.js'
+              code: "(" + modifyDOM + ")();",
+            },
+            (results) => {
+              const re = /^(.+?)<\//;
+              var problemDetails = re.exec(
+                results
+                  .toString()
+                  .split('<div data-cy="question-title" class="css-v3d350">')[1]
+              )[1];
+              var problemLevel = re.exec(
+                results.toString().split('class="css-dcmtd5">')[1]
+              )[1];
+              var problemNumber = problemDetails.split(".")[0].trim();
+              var problemDescription = problemDetails.split(".")[1].trim();
+              console.log(problemNumber);
+              console.log(problemDescription);
+              console.log(problemLevel);
+            }
+          );
+        } else {
+          document.getElementsByClassName("problems")[0].style.display = "none";
+          var message = document.querySelector("#message");
+          chrome.tabs.executeScript(
+            null,
+            {
+              file: "getPagesSource.js",
             },
             function () {
               if (chrome.runtime.lastError) {
-                message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+                message.innerText =
+                  "There was an error injecting script : \n" +
+                  chrome.runtime.lastError.message;
               }
-            });
+            }
+          );
         }
-      }
-      else {
-        document.getElementsByClassName('home')[0].style.display = 'none';
-        document.getElementsByClassName('problems')[0].style.display = 'none';
+      } else {
+        document.getElementsByClassName("home")[0].style.display = "none";
+        document.getElementsByClassName("problems")[0].style.display = "none";
       }
     }
-  )
+  );
 }
 
-window.onload = onWindowLoad
+window.onload = onWindowLoad;

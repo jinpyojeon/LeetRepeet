eafea0d4cbce865c06a3be428448febcf17c3f40
Mridul Ghanshala <mghansha@uwaterloo.ca>
2020-04-03 10:02:21 +0530

    Something

--- popup.js
+++ popup.js
@@ -2,10 +2,16 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
   if (request.action === "getSource") {
     message.innerText = request.source;
     const re = /^(.+?)\//;
-    const username = re
+    var username = re
       .exec(message.innerText.split("https://assets.leetcode.com/users/")[1])[1]
       .trim();
   }
+  chrome.storage.local.set({ "currentUsername": username },function() {
+  });
+  console.log(username);
+  chrome.storage.local.get(['currentUsername'], function (user) {
+    console.log('Value currently is ' + user.key);
+  });
 });
 
 function onWindowLoad() {
@@ -17,49 +23,53 @@ function onWindowLoad() {
     function (tabs) {
       var currentUrl = tabs[0].url;
       if (currentUrl.startsWith("https://leetcode.com/")) {
-        document.getElementsByClassName("other")[0].style.display = "none";
-        if (currentUrl.startsWith("https://leetcode.com/problems/")) {
-          document.getElementsByClassName("home")[0].style.display = "none";
-          function modifyDOM() {
-            return document.body.innerHTML;
+        chrome.storage.local.get(['currentUsername'], function (user) {
+          console.log('Value currently is ' + user.key);
+          document.getElementsByClassName("other")[0].style.display = "none";
+          if (currentUrl.startsWith("https://leetcode.com/problems/")) {
+            document.getElementsByClassName("home")[0].style.display = "none";
+            chrome.tabs.executeScript(
+                {
+                  code: "(" + modifyDOM + ")();",
+                },
+                (results) => {
+                  results = results.toString();
+                  var problemNumber = determineProblemNumber(results);
+                  var problemDescription = determineProblemDescription(results);
+                  var problemLevel = determineProblemLevel(results);
+                  var date = new Date().addDays(180);
+                  dbstuff(user, problemNumber, problemLevel, problemDescription, date);
+                  console.log(alasql("SELECT * FROM Information"));
+                }
+            );
+          } else {
+            document.getElementsByClassName("problems")[0].style.display = "none";
+            var message = document.querySelector("#message");
+            chrome.tabs.executeScript(
+                null,
+                {
+                  file: "getPagesSource.js",
+                },
+                function () {
+                  if (chrome.runtime.lastError) {
+                    message.innerText =
+                        "There was an error injecting script : \n" +
+                        chrome.runtime.lastError.message;
+                  }
+                }
+            );
           }
-          chrome.tabs.executeScript(
-            {
-              code: "(" + modifyDOM + ")();",
-            },
-            (results) => {
-              results = results.toString();
-              var problemNumber = determineProblemNumber(results);
-              var problemDescription = determineProblemDescription(results);
-              var problemLevel = determineProblemLevel(results);
-              var problemUsername = determineProblemUsername(results);
-            }
-          );
-          var date = new Date().addDays(180);
-        } else {
-          document.getElementsByClassName("problems")[0].style.display = "none";
-          var message = document.querySelector("#message");
-          chrome.tabs.executeScript(
-            null,
-            {
-              file: "getPagesSource.js",
-            },
-            function () {
-              if (chrome.runtime.lastError) {
-                message.innerText =
-                  "There was an error injecting script : \n" +
-                  chrome.runtime.lastError.message;
-              }
-            }
-          );
-        }
+        });
       } else {
         document.getElementsByClassName("home")[0].style.display = "none";
         document.getElementsByClassName("problems")[0].style.display = "none";
-        dbstuff();
       }
     }
   );
+}
+
+function modifyDOM() {
+  return document.body.innerHTML;
 }
 
 Date.prototype.addDays = function (days) {
@@ -78,11 +88,6 @@ function determineProblemLevel(results) {
     problemLevel = "Hard";
   }
   return problemLevel;
-}
-
-function determineProblemUsername(results){
-  const re2 = /^(.+?)\//;
-  return re2.exec(results.split('class="user-link__2Czf" href="/')[1])[1];
 }
 
 function determineProblemDescription(results)
@@ -105,12 +110,31 @@ function determineProblemNumber(results)
   )[1].split(".")[0].trim();
 }
 
-function dbstuff()
+function dbstuff(username, number, level, desc, date) {
+  createTables();
+  insertData(username, number, level, desc, date);
+}
+function createTables()
 {
-  alasql("CREATE TABLE IF NOT EXISTS cities (city string, population number)");
-  alasql("INSERT INTO cities VALUES ('Rome',2863223), ('Paris',2249975), ('Berlin',3517424),  ('Madrid',3041579)");
-  alasql("INSERT INTO cities VALUES ('Rome',2863223), ('Paris',2249975), ('Berlin',3517424),  ('Madrid',3041579)");
-  console.log(alasql("Select * from cities"));
+  alasql("CREATE TABLE IF NOT EXISTS Information (username STRING, problemno NUMBER, problemlevel STRING, problemdesc STRING, repeatdate DATE )");
+}
 
+function insertData(username, number, level, desc, date){
+  var ifExists = alasql("SELECT COUNT(*) FROM Information WHERE username = ? && problemno = ?",[username, number]);
+  if(ifExists > 0){
+    updateRecord(username, number, level, desc, date);
+  } else{
+    insertNewRecord(username, number, level, desc, date);
+  }
+}
+
+function insertNewRecord(username, number, level, desc, date)
+{
+  alasql("INSERT INTO Information (?, ?, ?, ?,?)",[username, number, level, desc, date]);
+
+}
+
+function updateRecord(username, number, level, desc, date){
+  alasql("UPDATE Information SET repeatdate = ? WHERE username = ? && problemno = ?",[date,username]);
 }
 window.onload = onWindowLoad;

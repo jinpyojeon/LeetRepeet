37552553d01b718605695758f00cb7e02a8b12af
Mridul Ghanshala <mghansha@uwaterloo.ca>
2020-04-08 16:29:34 +0530

    Added google analytics

--- popup.js
+++ popup.js
@@ -1,6 +1,15 @@
 var problemDate;
 var parent = document.getElementById("buttons");
 parent.addEventListener("click", myFunction);
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-163086542-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 function onWindowLoad() {
   chrome.tabs.query(
       {
@@ -194,7 +203,6 @@ function dbstuff(
       problemDescription,
       problemDate
   );
-  console.log(alasql("SELECT * FROM Information"));
 }
 
 function createTables() {
